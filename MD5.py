5858b79b2c337542011792e8fbe9f5cb8b1acd9f
米汤 <77874955+xk-mt@users.noreply.github.com>
2021-10-18 14:27:59 +0800

    Create MD5.py

---
+++ MD5.py
@@ -0,0 +1,12 @@
+
+import hashlib
+
+a = input('MD5加密文本：')
+m = hashlib.md5()
+for b in range(50):
+    m.update(a.encode(encoding='utf-8'))
+    a = m.hexdigest()
+    print('MD5加密密文：' + a)
+m.update(a.encode(encoding='utf-8'))
+a = m.hexdigest()
+input('MD5加密密文：' + a)

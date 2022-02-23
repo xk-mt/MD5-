e41f0f079024f0f36a12985689adf95fb8dfaa39
米汤 <77874955+xk-mt@users.noreply.github.com>
2022-02-23 18:27:21 +0800

    Update MD5.py

--- MD5.py
+++ MD5.py
@@ -1,7 +1,8 @@
 
 import hashlib
 
-a = input('MD5加密文本：')
+# a = input('MD5加密文本：')
+a='1'
 m = hashlib.md5()
 for b in range(50):
     m.update(a.encode(encoding='utf-8'))
@@ -9,4 +10,5 @@ for b in range(50):
     print('MD5加密密文：' + a)
 m.update(a.encode(encoding='utf-8'))
 a = m.hexdigest()
-input('MD5加密密文：' + a)
+# input('MD5加密密文：' + a)
+print('MD5加密密文：' + a)
